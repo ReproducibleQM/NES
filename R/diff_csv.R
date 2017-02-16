21b2ba94f224d07e0aa81fd41e6edb7f9b755631
jsta <jstachelek@utexas.edu>
2017-02-16 15:23:28 -0500

    use compareDF instead of daff

--- R/diff_csv.R
+++ R/diff_csv.R
@@ -1,4 +1,4 @@
-#' @import daff
+#' @import compareDF
 #' @examples \dontrun{
 #' diff_csv("475/res.csv", "475/res_review.csv")
 #' diff_csv("476/res.csv", "476/res_review.csv")
@@ -7,5 +7,9 @@ diff_csv <- function(original_csv, hand_edit_csv){
   orig_csv  <- read.csv(original_csv, stringsAsFactors = FALSE)
   hedit_csv <- read.csv(hand_edit_csv, stringsAsFactors = FALSE)
   
-  daff::render_diff(daff::diff_data(orig_csv, hedit_csv))
+  res <- compareDF::compare_df(hedit_csv, orig_csv, c("pagenum"))
+  res$change_summary
+  res$change_count
+  res$comparison_table_diff
+  write(res$html_output, "compareDF.html")
 }
