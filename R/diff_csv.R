bf71a26ce087e41f9c5959a27d7a7eac66abc5ab
jsta <jstachelek@utexas.edu>
2017-02-21 10:36:12 -0500

    report number of cells and lines changed

--- R/diff_csv.R
+++ R/diff_csv.R
@@ -8,8 +8,14 @@ diff_csv <- function(original_csv, hand_edit_csv){
   hedit_csv <- read.csv(hand_edit_csv, stringsAsFactors = FALSE)
   
   res <- compareDF::compare_df(hedit_csv, orig_csv, c("pagenum"))
-  res$change_summary
+  
   res$change_count
-  res$comparison_table_diff
-  write(res$html_output, "compareDF.html")
+  # head(res$comparison_table_diff, 7)
+  rows_changed <- res$change_summary[3]
+  cell_changes <- length(grep("\\+", unlist(res$comparison_table_diff[,3:ncol(res$comparison_table_diff)])))
+  percent_diff <- round(cell_changes / length(unlist(res$comparison_table_diff[,3:ncol(res$comparison_table_diff)])) * 100, 2)
+  
+  # write(res$html_output, "compareDF.html")
+  
+  paste0(cell_changes, " cells changed; ",rows_changed, " rows changed; ", percent_diff, "% percent difference")
 }
