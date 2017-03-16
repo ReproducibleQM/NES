85aaca0d9b4f2ab7fa58a51b59f390c4ad239ab3
jsta <jstachelek@utexas.edu>
2017-03-16 11:34:20 -0400

    add a dimension check

--- R/diff_csv.R
+++ R/diff_csv.R
@@ -1,14 +1,21 @@
 #' @import compareDF
 #' @examples \dontrun{
-#' diff_csv("474/res.csv", "474/res_review.csv")
+#' diff_csv("474/res.csv", "474/res_REVIEW.csv")
 #' diff_csv("475/res.csv", "475/res_review.csv")
 #' diff_csv("476/res.csv", "476/res_review.csv")
 #' diff_csv("477/res.csv", "477/res_review.csv")
 #' }
 diff_csv <- function(original_csv, hand_edit_csv){
   orig_csv  <- read.csv(original_csv, stringsAsFactors = FALSE)
-  hedit_csv <- read.csv(hand_edit_csv, stringsAsFactors = FALSE)
+  hedit_csv <- read.csv(hand_edit_csv, stringsAsFactors = FALSE)[,1:ncol(orig_csv)]
   
+  
+  # doctr::issues(doctr::compare(orig_csv, hedit_csv))
+  
+  # # daff ####
+  # res <- daff::diff_data(orig_csv, hedit_csv)
+  
+  # compareDF ####
   res <- compareDF::compare_df(hedit_csv, orig_csv, c("pagenum"))
   
   res$change_count
