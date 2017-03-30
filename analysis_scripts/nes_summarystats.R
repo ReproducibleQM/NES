35357e6836ee8702d50b9f03686414cd67e358e6
dustinkincaid <dustinkincaid@gmail.com>
2017-03-30 11:37:22 -0400

    update summary stats code

--- analysis_scripts/nes_summarystats.R
+++ analysis_scripts/nes_summarystats.R
@@ -6,6 +6,8 @@ nes_all <- read.csv("/Users/dustinkincaid/gReen2O/nes_data.csv", header=TRUE, na
 
 # temporary code: rename JERSEY to NEW JERSEY
 levels(nes_all$state)[levels(nes_all$state)=="JERSEY"] <- "NEW JERSEY"
+# temporary code: classify Lake Wissota in WI (Northeast) to IMPOUNDMENT
+nes_all$lake_type[nes_all$name %in% "LAKE WISSOTA"] <- "IMPOUNDMENT"
 
 # add a column to specify region
 nes_all[nes_all$state %in% c("VERMONT", "CONNECTICUT", "RHODE ISLAND", "NEW HAMPSHIRE", "NEW YORK", "MAINE", "MASSACHUSETTS", "WISCONSIN", "MINNESOTA", "MICHIGAN"), "region"] <- "NORTHEAST"
@@ -28,3 +30,18 @@ summary_nes = ddply(nal, .(variable, region, lake_type), summarize,
                 sd = sd(value, na.rm=TRUE),
                 N = length(value[!is.na(value)])
 )
+
+# now how to report numbers? let's convert them to character strings & round means & sd's to 2 decimal places for now
+summary_nes$mean <- as.character(round(summary_nes$mean, digits=2))
+summary_nes$sd <- as.character(round(summary_nes$sd, digits=2))
+summary_nes$N <- as.character(summary_nes$N)
+
+# add parentheses around N
+summary_nes$N <- paste("(", summary_nes$N, ")", sep="")
+
+# now create a column with the 3 values & the +- symbol (using paste?)
+summary_nes$stat <- paste(summary_nes$mean, summary_nes$sd, summary_nes$N, sep=" ")
+
+# how to make +- symbol
+x<-"Mean \u00b1 SD or N (%)"
+Encoding(x)<-"UTF-8"
