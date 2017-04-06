925ca1472b77484956d1e3283a4271ff29b33ba4
jsta <jstachelek@utexas.edu>
2017-04-06 10:19:12 -0400

    add retention time units to merged dataset, closes #8

--- 05_analysis_scripts/PDF_Merge.R
+++ 05_analysis_scripts/PDF_Merge.R
@@ -5,6 +5,7 @@ Path475 <- '03_qa_data//475//res_review.csv'
 Path476 <- '03_qa_data//476//res_review.csv'
 Path477 <- '03_qa_data//477//res_review.csv'
 StoretPath <- '04_supporting_data//storet.csv'
+retention_units <- '04_supporting_data/retention_time_units.csv'
 
 # Import Data -------------------------------------------------------------
 
@@ -15,6 +16,8 @@ df476 <- read.csv(Path476, header = TRUE, colClasses = rep("character", 34))
 df477 <- read.csv(Path477, header = TRUE, colClasses = rep("character", 34))
 dfStoret <- read.csv(StoretPath, header = TRUE, colClasses = c(rep("character", 6)))
 dfStoret <- dfStoret[, -c(1:3)]
+
+retention_units <- read.csv(retention_units, stringsAsFactors = FALSE)
 
 # Add Pdf Origin Column ---------------------------------------------------
 
@@ -28,10 +31,23 @@ df477$Pdf <- "477"
 df_final <- merge(df474, df475, all = TRUE)
 df_final <- merge(df_final, df476, all = TRUE)
 df_final <- merge(df_final, df477, all = TRUE)
-df_final <- merge(df_final, dfStoret, by.x = "storet_code", by.y = "Storet", all.x = TRUE)
-df_final <- df_final[, -2]
+df_final <- merge(df_final, dfStoret, by.x = "storet_code", by.y = "Storet", 
+                  all.x = TRUE)
+# df_final <- df_final[, -2]
 
 df_final$Long <- (-1 * as.numeric(df_final$Long))
+names(df_final) <- tolower(names(df_final))
+
+df_final <- merge(df_final, retention_units, by = c("pdf", "pagenum"), all.x = TRUE, all.y = FALSE)
+
+ret_time_pos <- which(names(df_final) == "retention_time")
+ret_time_units_pos <- which(names(df_final) == "retention_time_units")
+
+df_final <- df_final[,c(
+  1:ret_time_pos, 
+  ret_time_units_pos, 
+  (ret_time_pos + 1):(ncol(df_final) - 1)
+  )]
 
 # Export Dataframe as CSV -------------------------------------------------
 
