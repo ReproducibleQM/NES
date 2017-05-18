452ae345b13751d018097c42e8b69c664de10479
jsta <jstachelek@utexas.edu>
2017-05-18 12:43:25 -0400

    align plus minus signs, rm n values

--- 05_analysis_scripts/nes_summarystats.R
+++ 05_analysis_scripts/nes_summarystats.R
@@ -1,21 +1,28 @@
-# This code calculates a mean, standard deviation, and N for each measured
-# variable by region & lake type ##############################################
+# Calculate mean, standard deviation, and N for each measured variable
+# Setup ####
 
 nes_all <- read.csv("nes_data.csv", header=TRUE, na.strings="NA", stringsAsFactors = FALSE)
 
 nes_all[nes_all$state %in% c("VERMONT", "CONNECTICUT", "RHODE ISLAND", "NEW HAMPSHIRE", "NEW YORK", "MAINE", "MASSACHUSETTS", "WISCONSIN", "MINNESOTA", "MICHIGAN"), "region"] <- "Northeastern"
-nes_all[nes_all$state %in% c("ALABAMA", "DELAWARE", "FLORIDA", "GEORGIA", "ILLINOIS", "INDIANA", "KENTUCKY", "MARYLAND", "MISSISSIPPI", "NEW JERSEY", "NORTH CAROLINA", "OHIO", "PENNSYLVANIA", "SOUTH CAROLINA", "TENNESSEE", "VIRGINIA", "WEST VIRGINIA"), "region"] <- "Eastern"
+nes_all[nes_all$state %in% c("ALABAMA", "DELAWARE", "FLORIDA", "GEORGIA", "ILLINOIS", "INDIANA", "KENTUCKY", "MARYLAND", "MISSISSIPPI", "NEW JERSEY", "NORTH CAROLINA", "OHIO", "PENNSYLVANIA", "SOUTH CAROLINA", "TENNESSEE", "VIRGINIA", "WEST VIRGINIA"), "region"] <- "Southeastern"
 nes_all[nes_all$state %in% c("ARKANSAS", "IOWA", "KANSAS", "LOUISIANA", "MISSOURI", "NEBRASKA", "NORTH DAKOTA", "OKLAHOMA", "SOUTH DAKOTA", "TEXAS"), "region"] <- "Central"
 nes_all[nes_all$state %in% c("ARIZONA", "CALIFORNIA", "COLORADO", "IDAHO", "MONTANA", "NEVADA", "NEW MEXICO", "OREGON", "UTAH", "WASHINGTON", "WYOMING"), "region"] <- "Western"
 nes_all$region <- as.factor(nes_all$region)
-nes_all$region <- factor(nes_all$region, levels = c("Western", "Central", "Northeastern", "Eastern"))
+nes_all$region <- factor(nes_all$region, levels = c("Western", "Central", "Northeastern", "Southeastern"))
+nes_all$Region <- nes_all$region
 
-# reshape data to [n]es_[a]ll [l]ong format
+# convert days to years
+nes_all$retention_time[nes_all$retention_time_units == "days" & 
+                     !is.na(nes_all$retention_time_units)] <- 
+  nes_all$retention_time[nes_all$retention_time_units == "days" & 
+                       !is.na(nes_all$retention_time_units)] / 365
+
+# Reshape data to long format ####
 library(reshape2)
 
 nal <- melt(nes_all, 
             id.vars = c("state", "name", "region"),
-            measure.vars = c((names(nes_all)[c(9:37)])))
+            measure.vars = c((names(nes_all)[c(8:37)])))
 nal$value <- as.numeric(nal$value)
 
 library(plyr)
@@ -39,14 +46,14 @@ summary_nes$mean <- as.character(summary_nes$mean)
 summary_nes$sd   <- as.character(summary_nes$sd)
 summary_nes$N    <- as.character(summary_nes$N)
 
-summary_nes$N <- paste("(", summary_nes$N, ")", sep = "")
+# summary_nes$N <- paste("(", summary_nes$N, ")", sep = "")
 
 # Formatting with +- symbol ###################################################
 
-summary_nes$stat <- paste(summary_nes$mean, "\u00b1", summary_nes$sd, 
-                          summary_nes$N, sep=" ")
+summary_nes$stat <- paste(summary_nes$mean, "\u00b1", summary_nes$sd, sep=" ")
+
 summary_nes[is.na(summary_nes$sd), "stat"] <- 
-  paste(summary_nes[is.na(summary_nes$sd), "mean"], summary_nes[is.na(summary_nes$sd), "N"])
+  paste(summary_nes[is.na(summary_nes$sd), "mean"])
 summary_nes[summary_nes$mean == "NaN", "stat"] <- ""
 
 Encoding(summary_nes$stat) <- "UTF-8"
@@ -124,11 +131,74 @@ physchemdf <- subset(summary_nes,
                      summary_nes$variable_type == "PHYSIOCHEMICAL")
 loaddf     <- subset(summary_nes, summary_nes$variable_type == "LOADING")
 
-morph_wide <- dcast(morphdf, variable ~ region, value.var="stat")
-physchem_wide <- dcast(physchemdf, variable ~ region, value.var="stat")  
-load_wide <- dcast(loaddf, variable ~ region, value.var="stat")
+reorg_table <- function(dt, include_header = FALSE){
+  dt$param <- dt$variable
+  dt <- dt[,-which(names(dt) %in% c("variable", "stat", "variable_type", "N"))]
+  
+  vars <- as.character(unique(dt$param))
+  
+  dt <- melt(dt, id = c("param", "region"))
+  dt <- dt[order(dt$param, dt$region),]
+  dt <- t(dcast(dt, variable ~ param + region, value.var = "value"))
+  dt <- dt[-1,]
+  
+  res <- matrix(nrow = nrow(dt) / 4, ncol = 3 * 4)
+  res[,seq(2, ncol(res), by = 3)] <- "\u00b1"
+  
+  # Western
+  res[,1] <- dt[seq(1, nrow(dt), by = 4), 1]
+  res[,3] <- dt[seq(1, nrow(dt), by = 4), 2]
+  
+  # Central
+  res[,4] <- dt[seq(2, nrow(dt), by = 4), 1]
+  res[,6] <- dt[seq(2, nrow(dt), by = 4), 2]
+  
+  # Northeastern
+  res[,7] <- dt[seq(3, nrow(dt), by = 4), 1]
+  res[,9] <- dt[seq(3, nrow(dt), by = 4), 2]
+  
+  # Southeastern
+  res[,10] <- dt[seq(4, nrow(dt), by = 4), 1]
+  res[,12] <- dt[seq(4, nrow(dt), by = 4), 2]
+  
+  res <- data.frame(res, stringsAsFactors = FALSE)
+  
+  if(include_header){
+    res <- rbind(res, c("Mean", NA, "sd",
+                        "Mean", NA, "sd",
+                        "Mean", NA, "sd",
+                        "Mean", NA, "sd"))
+    res$Variable <- c(vars, NA)
+  }else{
+    res$Variable <- vars
+  }
+  
+  res <- res[,c(ncol(res), 1:(ncol(res) - 1))]
+  
+  if(include_header){
+    res <- res[c(nrow(res), 1:(nrow(res) - 1)), ]
+  }
+  
+  names(res) <- c(NA, 
+                  NA, "Western", NA, 
+                  NA, "Central", NA,
+                  NA, "Northeastern", NA,
+                  NA, "Southeastern", NA)
+  
+  res
+}
 
-# let's make the tables
+big_reorg <- rbind(reorg_table(morphdf, include_header = TRUE),
+                   reorg_table(physchemdf),
+                   reorg_table(loaddf))
+
+morph_wide    <- dcast(morphdf   , variable ~ region, value.var = c("mean", "sd"))
+physchem_wide <- dcast(physchemdf, variable ~ region, value.var = "stat")  
+load_wide     <- dcast(loaddf    , variable ~ region, value.var = "stat")
+big_wide      <- rbind(morph_wide, physchem_wide, load_wide)
+
+# Make the tables ####
+
 # install.packages("devtools")
 # devtools::install_github("rstudio/rmarkdown")
 # For dev version
@@ -145,6 +215,7 @@ sink_text <- function(dt, fname){
   write(file = fname, c("\\documentclass{article}",
                         "\\usepackage{booktabs}",
                         "\\usepackage{pdflscape}",
+                        "\\usepackage[table]{xcolor}",
                         "\\begin{document}",
                         "\\begin{landscape}",
                         gsub("±", "$\\\\pm$", dt),
@@ -161,6 +232,21 @@ sink_text((kable(physchem_wide, format = "latex", booktabs = T) %>%
 sink_text((kable(load_wide, format = "latex", booktabs = T) %>%
              kable_styling()), "07_tables/load_table.tex")
 
-sink_text((kable(rbind(morph_wide, physchem_wide, load_wide), 
-                 format = "latex", booktabs = T) %>%
+sink_text((kable(big_wide, format = "latex", booktabs = T) %>%
              kable_styling()), "07_tables/big_table.tex")
+
+names(big_reorg) <- big_reorg[1,]
+names(big_reorg)[1] <- "Variable"
+names(big_reorg)[seq(3, ncol(big_reorg), by = 3)] <- "\u00a0"
+
+sink_text(
+    (knitr::kable(big_reorg[-1,], 
+                  format = "latex", booktabs = T, row.names = FALSE) %>% 
+       kable_styling(latex_options = "striped") %>% 
+       add_header_above(c("Region", 
+                          "Western" = 3,
+                          "Central" = 3,
+                          "Northeastern" = 3,
+                          "Southeastern" = 3))), 
+  "07_tables/big_table_no_n.tex")
+
