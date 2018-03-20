771e0b8b3dd7ef81a187a961204ae81b043bbbfa
jsta <stachel2@msu.edu>
2018-03-20 11:48:09 -0400

    add chl to data product, closes #28

---
+++ 05_analysis_scripts/add_chl.R
@@ -0,0 +1,21 @@
+raw_paths <- c("../nesR/474/res.csv",
+               "../nesR/475/res.csv",
+               "../nesR/476/res.csv",
+               "../nesR/477/res.csv")
+surveys <- 474:477
+
+dt_raw <- lapply(seq_len(length(surveys)), function(x){
+  dt <- read.csv(raw_paths[x], stringsAsFactors = FALSE)
+  dt$survey <- surveys[x]
+  dt
+})
+
+dt <- do.call("rbind", dt_raw)
+dt <- dt[, c("survey", "pagenum", "chl")]
+write.csv(dt, "02_raw_data/merged_data/chl.csv", row.names = FALSE)
+
+nes   <- read.csv("nes_data.csv", stringsAsFactors = FALSE)
+dt_qa <- read.csv("03_qa_data/chl.csv", stringsAsFactors = FALSE)
+
+nes <- dplyr::left_join(nes, dt_qa, by = c("pdf" = "survey", "pagenum"))
+write.csv(nes, "nes_data.csv", row.names = FALSE)
